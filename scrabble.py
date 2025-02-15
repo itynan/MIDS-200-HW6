cc7b45caad4e62d04e9e24ccf987df41528eb8ed
iliya tynan <itynan@gmail.com>
2025-02-14 22:54:42 -0500

    update code

--- scrabble.py
+++ scrabble.py
@@ -1,8 +1,47 @@
+from collections import defaultdict
 from wordscore import score_word, powerset
 
+def create_word_dict():
+    with open("sowpods.txt","r") as infile:
+        raw_input = infile.readlines()
+        data = [datum.strip('\n') for datum in raw_input]
+    
+
+    #two pointer approach to populate dict more efficiently 
+    left = 0
+    right = len(data) - 1
+    mid = len(data) // 2
+    
+    word_dict = defaultdict(list)
+
+    '''key should be two letters since only 2 letter words and larger are used
+    https://www.geeksforgeeks.org/defaultdict-in-python/?utm_source=chatgpt.com
+    using default dict to create a dictionary of dictionaries organized by first 
+    two letters EX: {"AA": "AA","AARDVARK" } etc'''
+
+    while left <= mid and right > mid:
+        for i in [left,right]:
+            if(len(data[i])) >= 2 and (len(data[i])) < 8:
+                
+                d_key = data[i][:2]
+                word_dict[d_key].append(data[i])
+        left += 1
+        right -= 1
+
+    return word_dict
+
 def run_scrabble():
-    #print(score_word())
-    pass
+    word_dict = create_word_dict()
+    
+    words_to_check = powerset("MATTERED")
+    list_to_score = []
+    for word in words_to_check:
+        sub_dict = word_dict.get(word[:2],[])
+        if word in sub_dict:
+            list_to_score.append(word)
+    print(list_to_score)
+    #score_word(list_to_score)
+ 
 if __name__ == "__main__":
     run_scrabble()
-    print(powerset("super"))
+   
--- wordscore.py
+++ wordscore.py
@@ -1,6 +1,16 @@
 from itertools import chain, combinations
 
-def score_word():
+
+#will take each word and return the score to be imported into scabble.py
+def score_word(words):
+
+    output = []
+
+    for word in words:
+        t_word = tuple(word)
+        word_score 
+        for char in t_word:
+#aggreagte word score
 
     scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
             "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
@@ -8,18 +18,31 @@ def score_word():
             "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
             "x": 8, "z": 10}
     
+
+    
     return scores
 
 
 
+
+#TODO:FIGURE OUT WHAT TO DO WITH CASES IN TERMS OF INPUT AND SCORES DICT
+
 #taken from lecture 6.2.2, func returns the powerset of a word's characters
 def powerset(iterable):
-    if(type(iterable) == str):
-        iterable = tuple(iterable)
-        s = list(iterable)
-        return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
-    
+    if(type(iterable) != str or len(iterable) < 2):
+        raise TypeError("Input must be string with more than one character")
     else:
-        raise TypeError("Input must be string")
+        iterable = tuple(iterable.upper())
+        s = list(iterable)
+        all_tuples= list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
+            
+        #for loop constructs list of all possible permutations into contiguous words and removes tuples with <2 chars and > 7
+        words_to_check = []
+        for tupl in all_tuples:
+            word = "".join(tupl) 
+            if(len(word) > 1 and len(word) < 8):
+                words_to_check.append(word)
+        return words_to_check
+    
 
-
+#print(powerset("HAT"))
