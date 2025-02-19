190e507e7de544549bab4ee8add1cd63fb627a62
iliya tynan <itynan@gmail.com>
2025-02-18 21:25:36 -0500

    update logic for case of just wcs

--- scrabble.py
+++ scrabble.py
@@ -50,10 +50,10 @@ def run_scrabble(p_word):
     passes the the word into a powerset function that generates a list of all possible permutations.
     All permutations are tested against a dictionary of real words provided  '''
 
-    wc_case = [[],0]
+    
     if len(p_word) == 2 and ((p_word.count("*") + p_word.count("?"))==2):
         print(wc_case)
-        return wc_case
+        return[list([]),0]
     
     
     if any(char.isdigit() for char in p_word):
